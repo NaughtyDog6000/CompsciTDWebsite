3458753734a6753c4a879562e77027183a79cf1d
nd6k <robbie.monty@hotmail.com>
2024-01-27 17:46:08 +0000

    img for manifest

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,25 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
+      manifest: {
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
       devOptions: {
         enabled: true,
       },
