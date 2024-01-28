274352a253d936754bd522ab7dc224532c425ce3
nd6k <robbie.monty@hotmail.com>
2024-01-28 17:11:25 +0000

    removed un-used imports + declarations on signin page

--- src/Pages/Signin.tsx
+++ src/Pages/Signin.tsx
@@ -1,11 +1,11 @@
-import { UserTypeEnum, useAppState } from "@/Structs/State";
+// import { UserTypeEnum, useAppState } from "@/Structs/State";
 import { NavBar } from "@/components/NavBar";
 import { Helmet } from "react-helmet";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 export default function SigninPage(): JSX.Element {
-  const navigate = useNavigate();
-  const { AppState, SetAppState } = useAppState();
+  // const navigate = useNavigate();
+  // const { AppState, SetAppState } = useAppState();
   // if the user is signed in, redirect them to the home page as they cannot signin again
 
   return (
